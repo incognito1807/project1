15bd73740819d1093f88120abdaf0a803609089e
incognito1807 <tusupermanvodoi@gmail.com>
2020-10-29 12:39:38 +0700

    create react app

---
+++ Documents/Visual Studio 2010/Templates/ProjectTemplates/bài tập/javascript/hidepass.js
@@ -0,0 +1,10 @@
+var x = true;
+function hidePass() {
+    if(x){
+        document.getElementById('pass').type="text";
+        x = false;
+    } else {
+        document.getElementById('pass').type="password";
+        x = true;
+    }
+}
